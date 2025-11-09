48a63d451df768f11019ea5d5899373ef303cb47
subhaachyutha6-tech <subha.achyutha6@gmail.com>
2025-11-09 17:46:25 -0600

    EDA and PROBLEM STATEMENt

--- src/components/pipeline/exception.py
+++ src/components/pipeline/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging 
-from src.logger import logging
+from src.components.pipeline.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,12 +15,4 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_deatil=error_detail) 
 
 def __str__(self):
-    return self.error_message
-
-
-#if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by Zero ")
-        raise CustomException(e, sys)   
+    return self.error_message 
